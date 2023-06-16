fec335c194b68c71d90e130738cb2e613c63c9b5
jolokia106 <jolokia106.work@gmail.com>
2023-06-16 14:14:50 +0900

    位置調整

--- src/components/components/layouts/DefaultLayout.vue
+++ src/components/components/layouts/DefaultLayout.vue
@@ -4,27 +4,31 @@ import sidebar from '@/components/components/layouts/sidebar.vue';
 import header_view from '@/components/components/layouts/header.vue';
 import footer_view from '@/components/components/layouts/footer.vue';
 </script>
+
 <template>
-    <div id="default">
-
+    <!-- <div id="default"> -->
         <header_view />
-
-        <main>
-            <TheNavbar />
-            <div class="container-fluid">
-                <div class="row">
-
-                    <sidebar />
-
-                    <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                        <div class="ms-sm-auto">
-                            <router-view />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-
-        <footer_view />
+    <div class="row">
+        <div class="col-2">
+            <sidebar />
+        </div>
+        <div class="col-10"  style="padding-top: 90px;">
+            <router-view />
+        </div>
     </div>
+
+    <!-- <main> -->
+    <!-- <TheNavbar /> -->
+    <!-- <div class="container-fluid"> -->
+
+    <!-- <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4"> -->
+    <!-- <div class="ms-sm-auto"> -->
+    <!-- <router-view /> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </div> -->
+    <!-- </main> -->
+
+    <footer_view />
+    <!-- </div> -->
 </template>
